8466d15adb128ad14000b8caf22a3068a69284c7
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2021-08-11 09:28:23 -0400

    Day 1

---
+++ Day_1/main.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator");
+print("To get started, enter the name of the city you were grew up in:");
+city = input("");
+print("Now type the name of one of your pets. If you do not have a pet, type in your favorite animal:");
+pet = input("");
+print("");
+print("Generating band name");
+print("");
+print("Your band name is: \"The " + city + " " + pet + "\"");
